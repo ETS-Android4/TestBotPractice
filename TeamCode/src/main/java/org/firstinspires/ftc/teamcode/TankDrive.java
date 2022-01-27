0ce61122825172d2fb3705cce1aad330ada05981
amiller23 <amiller23@d-e.org>
2022-01-27 16:58:15 -0500

    1st

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankDrive.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+//import com.qualcomm.ftcrobotcontroller;
 
 @TeleOp
 public class TankDrive extends OpMode {
@@ -14,18 +15,23 @@ public class TankDrive extends OpMode {
 
     @Override
     public void init() {
-        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
-        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
-        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
-        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        motorFrontLeft = hardwareMap.dcMotor.get("FL");
+        motorBackLeft = hardwareMap.dcMotor.get("BL");
+        motorFrontRight = hardwareMap.dcMotor.get("FR");
+        motorBackRight = hardwareMap.dcMotor.get("BR");
+        //declaring
 
         motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        //setting right motors to be negative (so both sides run same direction)
     }
 
     @Override
     public void loop() {
-
+    motorFrontLeft.setPower(-gamepad1.left_stick_y);
+    motorBackLeft.setPower(-gamepad1.left_stick_y);
+    motorFrontRight.setPower(-gamepad1.right_stick_y);
+    motorBackRight.setPower(-gamepad1.right_stick_y);
     }
 }
 
